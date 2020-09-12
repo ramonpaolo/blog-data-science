bb85ee7ba765c8c1c1b25c47d977b673a138af52
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-09-12 13:23:06 -0300

    Add build

---
+++ build/precache-manifest.25d3692c2712a975afa0527e9b961e8a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1caff2bdf8e30461cf17c15175af4366",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b784d87485237588547e",
+    "url": "./static/css/main.f1aa9f8d.chunk.css"
+  },
+  {
+    "revision": "ae994819f5867baca7eb",
+    "url": "./static/js/2.a51e8acc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.a51e8acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b784d87485237588547e",
+    "url": "./static/js/main.0845dc04.chunk.js"
+  },
+  {
+    "revision": "989208dbd57b89fd121e",
+    "url": "./static/js/runtime-main.796b5bb3.js"
+  },
+  {
+    "revision": "84e5e9b6ea88f5eb8d09728ce827660f",
+    "url": "./static/media/arrow.84e5e9b6.svg"
+  },
+  {
+    "revision": "9ec02cd3785e9716693d586853b57394",
+    "url": "./static/media/data-science.9ec02cd3.apk"
+  },
+  {
+    "revision": "12483f964701cc42fab682081b52b70b",
+    "url": "./static/media/data_Science.12483f96.jpg"
+  },
+  {
+    "revision": "0b259cd7639f946d43135cc332ec2629",
+    "url": "./static/media/r.0b259cd7.png"
+  },
+  {
+    "revision": "54436f72cf74ee79c817f2fcf99efee3",
+    "url": "./static/media/toggle.54436f72.png"
+  }
+]);
