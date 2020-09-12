133edc834da600104782ef468bf3f30fc451898c
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-09-12 13:32:51 -0300

    Att routes

--- index.js
+++ index.js
@@ -32,35 +32,9 @@ const conecao = connect.query(
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-
-app.get("/content", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-app.use(bodyParser.json())
-app.get("/add_content", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-  console.log(req.body)
-});
-
-app.get("/add_content-button", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-  console.log(req.body)
-});
-
-app.get("/data", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-app.get("/help", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-app.get("/api", (req, res) => {
-  res.send(data);
-})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Ouvindo a porta: ${port}`));
