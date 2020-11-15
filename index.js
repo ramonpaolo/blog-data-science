92559f27b41ae9abbc668a4ad7995380d84eccf2
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-11-14 21:03:15 -0300

    Add mysql

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-//const sql = require("mysql");
+const sql = require("mysql");
 const bodyParser = require("body-parser")
 const data = require("./src/data/projects");
 const fs = require("fs");
 const { default: Axios } = require("axios");
+require('dotenv').config()
 
+console.log(process.env)
 
-/*const connect = sql.createConnection({
-  
+const connect = sql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER_DATABASE,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.PORT_SQL,
 });
+/*
 
+const connect = sql.createConnection({
+  host: "mysql669.umbler.com",
+  user: "ramon_paolo",
+  password: "familiAMaram12.",
+  database: "data-science",
+  port: 41890,
+});
+*/
 connect.connect((error) => { console.log(error) })
 
-connect.query("SELECT * FROM users", (erros, results, fields) => {
-  console.log("A solução é: " + results)
+connect.query("SELECT * FROM users", (error, results, fields) => {
+  if (error) {
+    console.log(erros)
+  } else if (results) {
+    console.log("Results: ")
+    results.forEach((user) => {
+      console.log(user)
+    })
+  } else {
+    console.log(fields)
+  }
 })
 
-const conecao = connect.query(
+/*
+connect.query(
   "SELECT * FROM `users`",
   (erros, results, fields) => {
     if (erros) {
       return console.log(erros);
     }
-    // console.log(results);
+     console.log(results);
     // console.log(fields);
     connect.end();
   }
